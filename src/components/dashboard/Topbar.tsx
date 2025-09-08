6d3a3ac28adf80b49f6a7a964e1b048d6f60b3b1
Yash Raj <kyashraj991@gmail.com>
2025-09-09 00:00:34 +0530

    feat: create toolbar

--- src/components/dashboard/Topbar.tsx
+++ src/components/dashboard/Topbar.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import {
+  CircleDashed,
+  Copy,
+  DiamondPlus,
+  PanelRightOpen,
+  Radio,
+} from 'lucide-react';
+import React from 'react';
 
 const Topbar = () => {
-  return (
-    <div className='border w-full'>Topbar</div>
-  )
-}
+  const items = [
+    {
+      icon: Copy,
+      label: 'All issues',
+    },
+    {
+      icon: Radio,
+      label: 'Active',
+    },
+    {
+      icon: CircleDashed,
+      label: 'Backlog',
+    },
+    {
+      icon: DiamondPlus,
+      label: null,
+    },
+  ];
 
-export default Topbar
+  return (
+    <div className="border gap-2 w-full flex p-1.5">
+      <div className="flex justify-center items-center hover:bg-zinc-900 hover:text-white text-zinc-500 rounded-lg transition-all px-4">
+        <PanelRightOpen size={19} strokeWidth={2.5} />
+      </div>
+      {items.map((i, idx) => (
+        <div
+          key={idx}
+          className="flex justify-center items-center gap-2 border px-2 py-1.5 hover:bg-zinc-900 hover:text-white text-zinc-500 rounded-md transition-all"
+        >
+          <span>{<i.icon size={17} strokeWidth={2.5} />}</span>
+          <p className="text-sm font-medium text-[#E3E4E7]">{i.label}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Topbar;
