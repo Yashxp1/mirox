5e6cc36752fd7805cabe30855cd68710c6b19afc
Yash Raj <kyashraj991@gmail.com>
2025-09-14 17:28:14 +0530

    feat: create api handler

---
+++ src/lib/apiHandler.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from './auth';
+
+// type HandlerResult<T> = T | NextResponse;
+
+type Hanlder<T> = (
+  req: NextRequest,
+  user: { id: string },
+  ctx: undefined
+) => Promise<T>;
+
+export function withApiHandler<T>(handler: Hanlder<T>) {
+  return async (req: NextRequest, ctx?: undefined) => {
+    try {
+      const session = await auth();
+
+      if (!session?.user?.id) {
+        return NextResponse.json(
+          { success: false, error: 'Unauthorized' },
+          { status: 401 }
+        );
+      }
+
+      const result = await handler(req, { id: session.user.id }, ctx);
+
+      // if (result instanceof NextResponse) {
+      //   return result;
+      // }
+
+      return NextResponse.json(
+        { success: true, data: result },
+        { status: 200 }
+      );
+    } catch (error) {
+      console.error('API ERROR:', error);
+      return NextResponse.json(
+        {
+          success: false,
+          error:
+            error instanceof Error ? error.message : 'Internal server error',
+        },
+        { status: 500 }
+      );
+    }
+  };
+}
