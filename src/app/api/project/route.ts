2d0d2277a8bfbc9adf24008fabc669fb6012d23b
Yash Raj <kyashraj991@gmail.com>
2025-09-14 20:58:07 +0530

    feat: configure task api

---
+++ src/app/api/project/route.ts
@@ -0,0 +1,69 @@
+import { withApiHandler } from '@/lib/apiHandler';
+import { prisma } from '@/lib/prisma';
+import { ProjectSchema, UpdateProjectSchema } from '@/lib/schema';
+import { NextRequest } from 'next/server';
+
+const createProject = async (req: NextRequest, user: { id: string }) => {
+  const body = await req.json();
+  const validatedData = ProjectSchema.parse(body);
+
+  const project = await prisma.project.create({
+    data: {
+      title: validatedData.title,
+      summary: validatedData.summary,
+      description: validatedData.description,
+      status: validatedData.status,
+      priority: validatedData.priority,
+      startdate: validatedData.startdate,
+      target: validatedData.target,
+      author: { connect: { id: user.id } },
+    },
+  });
+
+  return project;
+};
+
+const updateProject = async (req: NextRequest, user: { id: string }) => {
+  const body = await req.json();
+  const validatedData = UpdateProjectSchema.parse(body);
+
+  const updateTask = await prisma.task.update({
+    where: { id: validatedData.id },
+    data: {
+      title: validatedData.title,
+      description: validatedData.description,
+      startdate: validatedData.startdate,
+      target: validatedData.target,
+      status: validatedData.status,
+      priority: validatedData.priority,
+      author: { connect: { id: user.id } },
+    },
+  });
+
+  return updateTask;
+};
+const deleteProject = async (req: NextRequest) => {
+  const { projectId } = await req.json();
+
+  if (!projectId) {
+    throw new Error('Project Id not found');
+  }
+
+  const deleteProject = await prisma.project.delete({
+    where: { id: projectId },
+  });
+
+  return deleteProject;
+};
+const getProject = async (req: NextRequest, user: { id: string }) => {
+  const project = await prisma.project.findMany({
+    where: { authorId: user.id },
+  });
+
+  return project;
+};
+
+export const POST = withApiHandler(createProject);
+export const PUT = withApiHandler(updateProject);
+export const DELETE = withApiHandler(deleteProject);
+export const GET = withApiHandler(getProject);
