090a5faa117f5d8b48e56c333dad3d3fa5643d6b
Yash Raj <kyashraj991@gmail.com>
2025-09-18 22:39:23 +0530

    fix: project interface

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,14 +1,15 @@
 export interface Project {
-  id: number
-  title: string
-  summary: string | null
-  description: string | null
-  startdate: string | null
-  target: string | null
-  priority: 'NONE' | 'LOW' | 'MEDIUM' | 'HIGH'
-  status: 'PLANNED' | 'IN_PROGRESS' | 'DONE'
-  authorId: string
-  workspaceId: number
-  createdAt: string
-  updatedAt: string
+  id: number;
+  name: string;
+  title: string;
+  summary: string | null;
+  description: string | null;
+  startdate: string | null;
+  target: string | null;
+  priority: 'NONE' | 'LOW' | 'MEDIUM' | 'HIGH';
+  status: 'PLANNED' | 'IN_PROGRESS' | 'DONE';
+  authorId: string;
+  workspaceId: number;
+  createdAt: string;
+  updatedAt: string;
 }
