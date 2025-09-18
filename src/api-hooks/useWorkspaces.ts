d2e9f33fca012525ea0d7d9830857d8fe629d9d6
Yash Raj <kyashraj991@gmail.com>
2025-09-18 22:40:47 +0530

    feat: create workspace api call

--- src/api-hooks/useWorkspaces.ts
+++ src/api-hooks/useWorkspaces.ts
@@ -1,20 +1,49 @@
-// src/api-hooks/useWorkspaces.ts
-import { ApiResponse, Workspace } from '@/types/workspace';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+import { workspaceApi } from '@/lib/api';
+import { Workspace } from '@/types/workspace';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-const fetchWorkspaces = async (): Promise<Workspace[]> => {
-  const res = await axios.get<ApiResponse<Workspace[]>>(
-    'http://localhost:3000/api/workspace'
-  );
-
-  return res.data.data;
-};
-
-export function useWorkspaces() {
+//------ReadAll------
+export function useGetAllWorkspaces() {
   return useQuery<Workspace[]>({
     queryKey: ['workspaces'],
-    queryFn: fetchWorkspaces,
-    staleTime: 5 * 60
+    queryFn: workspaceApi.getAll,
+    staleTime: 5 * 60 * 1000,
+  });
+}
+
+//------ReadOne------
+export function useGetOneWorkspaces(id: number) {
+  return useQuery<Workspace>({
+    queryKey: ['workspaces', id],
+    queryFn: () => workspaceApi.getOne(id),
+    staleTime: 5 * 60 * 1000,
+  });
+}
+
+//------Create------
+export function useCreateWorkspaces() {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: workspaceApi.create,
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['workspaces'] }),
+  });
+}
+
+//------Update------
+export function useUpdateWorkspaces(id: number) {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: (payload: Partial<Workspace>) =>
+      workspaceApi.update(id, payload),
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['workspaces'] }),
+  });
+}
+
+//------Delete------
+export function useRemoveWorkspaces(id: number) {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: () => workspaceApi.remove(id),
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['workspaces'] }),
   });
 }
