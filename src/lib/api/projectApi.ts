181d012673d8e5d4e7d50c83ea8c570e6c9a954b
Yash Raj <kyashraj991@gmail.com>
2025-09-22 21:59:13 +0530

    feat: create api client

---
+++ src/lib/api/projectApi.ts
@@ -0,0 +1,52 @@
+import { Project } from '@/types/project';
+
+const baseURL = '/api/workspace';
+
+export const projectApi = {
+  getAll: async (workspaceId: string | number): Promise<Project[]> => {
+    const res = await axios.get<{ data: Project[] }>(
+      `${baseURL}/${workspaceId}/`
+    );
+    return res.data.data;
+  },
+
+  getOne: async (
+    workspaceId: string | number,
+    projectId: string | number
+  ): Promise<Project> => {
+    const res = await axios.get<{ data: Project }>(
+      `${baseURL}/${workspaceId}/${projectId}`
+    );
+    return res.data.data;
+  },
+
+  create: async (
+    workspaceId: string | number,
+    payload: Omit<Project, 'id'>
+  ): Promise<Project> => {
+    const res = await axios.post<{ data: Project }>(
+      `${baseURL}/${workspaceId}`,
+      payload
+    );
+    return res.data.data;
+  },
+
+  update: async (
+    workspaceId: string | number,
+    projectId: string | number,
+    payload: Partial<Project>
+  ): Promise<Project> => {
+    const res = await axios.put<{ data: Project }>(
+      `${baseURL}/${workspaceId}/${projectId}`,
+      payload
+    );
+    return res.data.data;
+  },
+
+  remove: async (
+    workspaceId: string | number,
+    projectId: string | number
+  ): Promise<void> => {
+    await axios.delete(`${baseURL}/${workspaceId}/${projectId}`);
+  },
+};
---
+++ src/lib/api/workspaceApi.ts
@@ -0,0 +1,39 @@
+import { Workspace } from '@/types/workspace';
+import axios from 'axios';
+
+const baseURL = '/api/workspace';
+
+export const workspaceApi = {
+  
+  getAll: async (): Promise<Workspace[]> => {
+    const res = await axios.get<{ data: Workspace[] }>(`${baseURL}/`);
+    return res.data.data;
+  },
+
+  getOne: async (workspaceId: string | number): Promise<Workspace> => {
+    const res = await axios.get<{ data: Workspace }>(
+      `${baseURL}/${workspaceId}`
+    );
+    return res.data.data;
+  },
+
+  create: async (payload: Omit<Workspace, 'id'>): Promise<Workspace> => {
+    const res = await axios.post<{ data: Workspace }>(`${baseURL}`, payload);
+    return res.data.data;
+  },
+
+  update: async (
+    workspaceId: string | number,
+    payload: Partial<Workspace>
+  ): Promise<Workspace> => {
+    const res = await axios.put<{ data: Workspace }>(
+      `${baseURL}/${workspaceId}`,
+      payload
+    );
+    return res.data.data;
+  },
+
+  remove: async (workspaceId: string | number): Promise<void> => {
+    await axios.delete(`${baseURL}/${workspaceId}/`);
+  },
+};
