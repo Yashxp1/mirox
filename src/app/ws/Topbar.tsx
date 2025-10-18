8fc834e299f8bbcb4bb4a834e6078627af9e70cd
Yash Raj <kyashraj991@gmail.com>
2025-10-18 21:47:44 +0530

    feat: customise topbar for doc editor route

--- src/app/ws/Topbar.tsx
+++ src/app/ws/Topbar.tsx
@@ -1,16 +1,34 @@
+import { useGetOnedoc } from '@/api-hooks/useDocuments';
 import Profile from '@/components/auth/Profile';
 import { ModeToggle } from '@/components/darkmode/ModeToggle';
+import { Spinner } from '@/components/ui/shadcn-io/spinner';
 import { Bell, Home } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
-const Topbar = () => {
+interface TopbarProps {
+  wsId: string;
+  docId: string;
+}
+
+const Topbar = ({ wsId, docId }: TopbarProps) => {
+  const { data, isLoading, isError } = useGetOnedoc(wsId, docId);
+
+  console.log('doc data: ', data);
+
   return (
     <div className=" dark:bg-[#222120] bg-zinc-50 shadow-2xs  w-full flex justify-between items-center py-2 dark:text-zinc-200 text-zinc-700 px-2 border-b">
-      <Link href='/dashboard'>
+      <Link href={`/ws/${wsId}`}>
         <Home className="size-5" />
       </Link>
-
+      {isLoading ? (
+        <Spinner variant="circle" />
+      ) : (
+        <h1 className="text-sm font-semibold">
+          {data?.name || 'Untitled document'}
+        </h1>
+      )}
+      {isError && <h1>Failed to load the name</h1>}
       <div className="flex justify-center items-center gap-4">
         <ModeToggle />
         <Bell className="size-5" />
