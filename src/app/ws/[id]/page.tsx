8357d77a39dbef3b99a629cf18fa4a9649c27295
Yash Raj <kyashraj991@gmail.com>
2025-10-18 21:49:34 +0530

    feat: get wsid

--- src/app/ws/[id]/page.tsx
+++ src/app/ws/[id]/page.tsx
@@ -1,11 +1,12 @@
 import Items from '@/app/dashboard/Items';
 import Sidebar from '@/components/Sidebar/Sidebar';
-import Topbar from '../Topbar';
 import DocUtils from '../DocUtils';
+import Topbar from '@/app/dashboard/Topbar';
 
-const Page = async ({ params }: { params: { id: string } }) => {
+const Page = async ({ params }: { params: { id: string; doc: number } }) => {
   const param = await params;
-  const id = param.id;
+  const wsId = param.id;
+  // const docId = param.doc;
 
   return (
     <div className="flex w-full">
@@ -15,12 +16,12 @@ const Page = async ({ params }: { params: { id: string } }) => {
       <div className="w-[300px] border-r  pt-[46px]">
         <Sidebar />
       </div>
-      <div className='flex flex-col w-full'>
-        <div className='w-full'>
-          <DocUtils />
+      <div className="flex flex-col w-full">
+        <div className="w-full">
+          <DocUtils workspaceId={wsId} />
         </div>
         <div className="p-10 w-full">
-          <Items workspaceId={id} />
+          <Items workspaceId={wsId} />
         </div>
       </div>
     </div>
