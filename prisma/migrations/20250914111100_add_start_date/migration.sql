91e35b790c46cf4b290d253cde53d47c9d193164
Yash Raj <kyashraj991@gmail.com>
2025-10-21 15:46:18 +0530

    add migrations

--- prisma/migrations/20250914111100_add_start_date/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `startdata` on the `Task` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Task" DROP COLUMN "startdata",
-ADD COLUMN     "startdate" TIMESTAMP(3);
