b854940872ed7548780a0a0c29693e1ecc729739
Yash Raj <kyashraj991@gmail.com>
2025-10-23 22:22:25 +0530

    fix: ui hover color tweaks

--- src/app/(home)/workspace/[id]/[taskId]/page.tsx
+++ src/app/(home)/workspace/[id]/[taskId]/page.tsx
@@ -3,19 +3,13 @@ import { useOneTasks } from '@/api-hooks/useTasks';
 import { Spinner } from '@/components/ui/spinner';
 import { Textarea } from '@/components/ui/textarea';
 import { useParams } from 'next/navigation';
-import React from 'react';
 
 const Page = () => {
   const params = useParams();
   const id = params.id as string;
   const taskId = params.taskId as string;
 
-  console.log('taskId ===> ', taskId);
-  console.log('wsId ===> ', id);
-
   const { data, isError, isLoading } = useOneTasks(id, taskId);
-
-  // console.log('Data: ===>', data);
 
   if (isError) return <p>An error occured</p>;
 
@@ -29,13 +23,22 @@ const Page = () => {
         ) : (
           <div className=" w-full px-40 py-12">
             {/* <h1 className="flex font-semibold text-xl">{data?.title}</h1> */}
-            <input className="flex font-semibold text-xl outline-0 w-full" value={data?.title} />
-            <p className="flex text-sm py-6">{data?.description}</p>
+            <input
+              className="flex font-semibold text-xl outline-0 w-full"
+              value={data?.title ?? ''}
+            />
+            {/* <p className="flex text-sm py-6">{data?.description}</p> */}
+            <textarea
+              value={data?.description ?? ''}
+              className="flex text-sm py-6 outline-0 w-full"
+            />
             {/* <hr /> */}
             <div className="flex pt-6 text-xs gap-2">
               <div className="flex flex-col">
-                <span className="text-xs text-zinc-400 mb-1">Created</span>
-                <p className="flex  border py-1 px-2 rounded-md bg-zinc-900 hover:bg-zinc-800 transition-all cursor-default">
+                <span className="text-xs text-muted-foreground mb-1">
+                  Created
+                </span>
+                <p className="flex border py-1 px-2 rounded-md bg-blue-500/20 hover:bg-blue-500/30 text-blue-700 dark:text-blue-300 border-blue-500/50 transition-all cursor-default">
                   {data?.createdAt
                     ? new Date(data?.createdAt).toLocaleString('en-US', {
                         day: 'numeric',
@@ -46,8 +49,10 @@ const Page = () => {
                 </p>
               </div>
               <div className="flex flex-col">
-                <span className="text-xs text-zinc-400 mb-1">Start Date</span>
-                <p className="flex  border py-1 px-2 rounded-md bg-zinc-900 hover:bg-zinc-800 transition-all cursor-default">
+                <span className="text-xs text-muted-foreground mb-1">
+                  Start Date
+                </span>
+                <p className="flex border py-1 px-2 rounded-md bg-green-500/20 hover:bg-green-500/30 text-green-700 dark:text-green-300 border-green-500/50 transition-all cursor-default">
                   {data?.startdate
                     ? new Date(data?.startdate).toLocaleString('en-US', {
                         day: 'numeric',
@@ -58,8 +63,10 @@ const Page = () => {
                 </p>
               </div>
               <div className="flex flex-col">
-                <span className="text-xs text-zinc-400 mb-1">Target</span>
-                <p className="flex  border py-1 px-2 rounded-md bg-zinc-900 hover:bg-zinc-800 transition-all cursor-default">
+                <span className="text-xs text-muted-foreground mb-1">
+                  Target
+                </span>
+                <p className="flex border py-1 px-2 rounded-md bg-purple-500/20 hover:bg-purple-500/30 text-purple-700 dark:text-purple-300 border-purple-500/50 transition-all cursor-default">
                   {data?.target
                     ? new Date(data?.target).toLocaleString('en-US', {
                         day: 'numeric',
@@ -70,15 +77,19 @@ const Page = () => {
                 </p>
               </div>
               <div className="flex flex-col">
-                <span className="text-xs text-zinc-400 mb-1">Priority</span>
-                <p className="flex  border py-1 px-2 rounded-md bg-zinc-900 hover:bg-zinc-800 transition-all cursor-default">
+                <span className="text-xs text-muted-foreground mb-1">
+                  Priority
+                </span>
+                <p className="flex border py-1 px-2 rounded-md bg-red-500/20 hover:bg-red-500/30 text-red-700 dark:text-red-300 border-red-500/50 transition-all cursor-default">
                   {data?.priority}
                 </p>
               </div>
               <div className="flex flex-col">
-                <span className="text-xs text-zinc-400 mb-1">Status</span>
-                <p className="flex  border py-1 px-2 rounded-md bg-zinc-900 hover:bg-zinc-800 transition-all cursor-default">
-                  {data?.updatedAt ? 'edited' : '---------------'}
+                <span className="text-xs text-muted-foreground mb-1">
+                  Status
+                </span>
+                <p className="flex border py-1 px-2 rounded-md bg-yellow-500/20 hover:bg-yellow-500/30 text-yellow-700 dark:text-yellow-300 border-yellow-500/50 transition-all cursor-default">
+                  {data?.status}
                 </p>
               </div>
             </div>
--- src/components/dashboard/Topbar.tsx
+++ src/components/dashboard/Topbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { ClipboardList, ListTodo, PanelRightOpen } from 'lucide-react';
 import React from 'react';
+import { ModeToggle } from '../darkmode/ModeToggle';
 
 interface TopbarProps {
   toggleSidebar: () => void;
@@ -8,22 +9,29 @@ interface TopbarProps {
 
 const Topbar: React.FC<TopbarProps> = ({ toggleSidebar }) => {
   return (
-    <div className="border border-l-0 gap-2 w-full flex p-1.5">
-      <div
-        onClick={toggleSidebar}
-        className="flex justify-center items-center hover:bg-zinc-900 hover:text-white text-zinc-500 rounded-lg transition-all px-4 cursor-pointer"
-      >
-        <PanelRightOpen size={19} strokeWidth={2.5} />
-      </div>
+    <div className="border-b py-1 w-full flex">
+      <div className="flex w-full justify-between">
+        <div className="flex py-1">
+          <div
+            onClick={toggleSidebar}
+            className="flex justify-center items-center hover:bg-zinc-900 hover:text-white text-zinc-500 rounded-lg transition-all px-4 cursor-pointer"
+          >
+            <PanelRightOpen size={16} strokeWidth={2.5} />
+          </div>
 
-      <div className="flex gap-2">
-        <div className="flex cursor-default justify-center items-center gap-2 border px-2 py-1.5 hover:bg-zinc-900 hover:text-white rounded-md transition-all">
-          <span>{<ClipboardList size={16} />}</span>
-          <p className="text-xs">All issues</p>
+          <div className="flex text-xs gap-2">
+            <div className="flex cursor-default justify-center items-center gap-2 border px-2 hover:bg-zinc-900 hover:text-white rounded-md transition-all">
+              <span>{<ClipboardList size={16} />}</span>
+              <p className="">All issues</p>
+            </div>
+            <div className="flex cursor-default justify-center items-center gap-2 border px-2 hover:bg-zinc-900 hover:text-white rounded-md transition-all">
+              <span>{<ListTodo size={16} />}</span>
+              <p className="">Assigned</p>
+            </div>
+          </div>
         </div>
-        <div className="flex cursor-default justify-center items-center gap-2 border px-2 py-1.5 hover:bg-zinc-900 hover:text-white rounded-md transition-all">
-          <span>{<ListTodo size={16} />}</span>
-          <p className="text-xs">Assigned</p>
+        <div className='px-4'>
+          <ModeToggle />
         </div>
       </div>
     </div>
